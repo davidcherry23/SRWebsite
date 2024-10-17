eca51bd216ffad64c54870c269c0d4802c08480f
davidcherry23 <davidcherry23@hotmail.co.uk>
2024-10-17 21:35:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = 'AIzaSyBfoy9gpe6UHjolsmoi9kAx-iapdYs1-_U'; // Your API Key
-const SPREADSHEET_ID = '1SPtzIaQaRVwbioFzIjE40QYtk3mAbzz2SS64W7ZdE3s'; // Your Spreadsheet ID
+const SPREADSHEET_ID = '1mcM8kiRXekXqBcPv9pLEUM3r8CR1R6TPROh2WW1ElSc'; // Your Spreadsheet ID
 
 async function fetchData(sheetName) {
     const range = `${sheetName}!A1:Z`; // Fetch all rows in the range

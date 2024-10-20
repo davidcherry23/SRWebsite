5920d6d062fa8b4c5297857ae49acf02d9d28709
davidcherry23 <davidcherry23@hotmail.co.uk>
2024-10-20 11:22:43 +0100

    Update ratings.js

--- ratings.js
+++ ratings.js
@@ -1,4 +1,4 @@
-const spreadsheetId = "1qPWKEkJqM29S_53OMH2isA7yKpVNUe4xeE5CQWJIdVY";
+const spreadsheetId = "1QWuc_WyWIeleLpBALFiCd2pD6KqEoYDKN5oD8bnbwyk";
 const apiKey = "AIzaSyBfoy9gpe6UHjolsmoi9kAx-iapdYs1-_U";
 const flatRange = "FLAT!A2:O800"; // Adjust range if needed
 const nhRange = "NH!A2:O600"; 

012ff4e38e5e4b1dc30c3d339ebbf555ea087938
davidcherry23 <davidcherry23@hotmail.co.uk>
2024-10-20 22:11:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = 'AIzaSyBfoy9gpe6UHjolsmoi9kAx-iapdYs1-_U'; // Your API Key
-const SPREADSHEET_ID = '1QWuc_WyWIeleLpBALFiCd2pD6KqEoYDKN5oD8bnbwyk'; // Your Spreadsheet ID
+const SPREADSHEET_ID = '1JZaY6ulx8vuAsMV8IeP_yLhG5XLT5fe_Zbk4ZOHa9Ok'; // Your Spreadsheet ID
 
 async function fetchData(sheetName) {
     const range = `${sheetName}!A1:Z`; // Fetch all rows in the range
